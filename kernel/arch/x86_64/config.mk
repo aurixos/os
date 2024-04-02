84e26ce5320b8a644c68af37f92d6ae4393ca4dc
Jozef Nagy <schkwve@gmail.com>
2024-04-02 21:11:35 +0200

    Update x86_64 kernel flags

--- kernel/arch/x86_64/config.mk
+++ kernel/arch/x86_64/config.mk
@@ -10,16 +10,23 @@ override INTERNAL_CFLAGS := -std=c11 \
 							-ffreestanding \
 							-fno-stack-protector \
 							-fno-stack-check \
+							-fno-lto \
 							-fpie \
 							-m64 \
+							-march=x86-64 \
 							-mabi=sysv \
 							-mno-80387 \
 							-mno-mmx \
 							-mno-sse \
 							-mno-sse2 \
 							-mno-red-zone \
-							-MMD
+							-MMD \
+							-MP
 
 override INTERNAL_LDFLAGS := -nostdlib \
+							--no-dynamic-linker \
 							-z max-page-size=4096 \
-							-static
+							-z text \
+							-melf_x86_64 \
+							-static \
+							-pie
