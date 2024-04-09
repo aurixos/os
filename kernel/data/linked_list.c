9e72441067b445030442197b575b22ebbc004eae
Jozef Nagy <schkwve@gmail.com>
2024-04-09 21:50:29 +0200

    Added linked list skeleton

---
+++ kernel/data/linked_list.c
@@ -0,0 +1,1 @@
+#include <data/linked_list.h>
---
+++ kernel/include/data/linked_list.h
@@ -0,0 +1,9 @@
+#ifndef _DATA_LINKED_LIST_H
+#define _DATA_LINKED_LIST_H
+
+struct linked_list {
+	struct linked_list *prev;
+	struct linked_list *next;
+};
+
+#endif /* _DATA_LINKED_LIST_H */
