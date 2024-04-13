c8470238fd124591485d2ae02dc177ad65d1fcfe
Jozef Nagy <schkwve@gmail.com>
2024-04-13 12:35:21 +0200

    Added better HDD generation for x86_64

--- arch/x86_64/config.mk
+++ arch/x86_64/config.mk
@@ -1,2 +1,4 @@
 QEMU := qemu-system-x86_64
 QEMU_ARCH_FLAGS := -bios arch/$(ARCH)/ovmf_x64.fd
+
+ARCH_RELEASE_HDD_RECOMMENDED_SIZE := 256
--- arch/x86_64/generate_hdd.sh
+++ arch/x86_64/generate_hdd.sh
@@ -5,15 +5,78 @@ if [[ -z $1 ]]; then
 	exit 1
 fi
 
-tempwdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'aurix')
-pushd $tempwdir >/dev/null 2>&1
+disk_name=$1
+disk_size=$2
 
-# TODO: Make a more robust generation script
-dd if=/dev/zero of=$1 bs=1k count=1440 2>/dev/null
-mformat -i $1 ::
-mcopy -s -i $1 $BUILD_DIR/output/* $ROOT_DIR/sysroot/* ::
+minimum_disk_size=128
+
+unamestr=$(uname)
+
+# Check if we're running on a supported host
+if ! [[ "$unamestr" =~ ^('Linux'|'Darwin') ]]; then
+	printf " failed. (unsupported host)\n"
+	exit 128
+fi
+
+if [[ $disk_size -lt $minimum_disk_size ]]; then
+	printf " failed. (specified disk size is less than minimum)\n"
+	printf "Specified size: $disk_size\n"
+	printf "Minimum size: $minimum_disk_size\n"
+	exit 1
+fi
+
+# create a disk image
+dd if=/dev/zero of="$disk_name" bs=1M count="$disk_size" >/dev/null 2>&1
+
+# create a partition table
+gdisk "$disk_name" >/dev/null 2>&1 << EOF
+o
+y
+n p
+1
+
++128M
+ef00
+w
+y
+EOF
+
+tempmountdir=$(mktemp -d 2>/dev/null)
+
+# mount disk
+if [ "$unamestr" = 'Linux' ]; then
+	echo "Linux HDD Generation not implemented yet"
+	rm -r $tempmountdir
+	exit 200
+elif [ "$unamestr" = 'Darwin' ]; then
+	loopback=$(hdiutil attach -nomount $disk_name)
+	loopback=$(echo $loopback | cut -f1 -d" ")
+
+	loopback_efi="$loopback"s1
+fi
+
+# format EFI partition
+mkfs.vfat -F32 -n "EFI" "$loopback_efi" >/dev/null 2>&1
+
+# mount EFI partition
+if [ "$unamestr" = 'Darwin' ]; then
+	diskutil mount -mountPoint "$tempmountdir" "$loopback_efi" >/dev/null 2>&1
+else
+	mount "$loopback_efi" "$tempmountdir"
+fi
+
+cp -r "$BUILD_DIR"/output/* "$tempmountdir"
+cp -r "$ROOT_DIR"/sysroot/* "$tempmountdir"
+
+# unmount all partitions
+if [ "$unamestr" = 'Linux' ]; then
+	echo "Linux HDD Generation not implemented yet"
+	rm -r $tempmountdir
+	exit 200
+elif [ "$unamestr" = 'Darwin' ]; then
+	diskutil unmountDisk "$loopback" >/dev/null 2>&1
+fi
+
+rm -r "$tempmountdir"
 
 printf " done.\n"
-
-popd >/dev/null 2>&1
-rm -r $tempwdir
