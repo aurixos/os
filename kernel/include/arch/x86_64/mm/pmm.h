f3e254587a77109eed1397be8e75f4c83898bbe6
Jozef Nagy <schkwve@gmail.com>
2024-04-15 15:48:45 +0200

    Get rid of memory management for now

--- kernel/include/arch/x86_64/mm/pmm.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ARCH_MM_PMM_H
-#define _ARCH_MM_PMM_H
-
-#define MAX_LAYER_COUNT 32
-
-#define BLOCK_SIZE 0x1000
-
-#endif /* _ARCH_MM_PMM_H */
--- kernel/kinit.c
+++ kernel/kinit.c
@@ -1,12 +1,11 @@
 #include <arch/cpu.h>
 #include <boot/boot.h>
-#include <mm/pmm.h>
 #include <aurix.h>
 
 void _start(void)
 {
 	cpu_init();
-	pmm_init(memmap_request.response);
+
 	klog("kernel init done");
 	for (;;);
 }
