381e3d38314b7261b8171af2c136193485c110b8
Jozef Nagy <schkwve@gmail.com>
2024-04-24 18:47:01 +0200

    Make bitmap functions always inlined

--- kernel/data/bmap.c
+++ kernel/data/bmap.c
@@ -1,16 +1,19 @@
 #include <data/bmap.h>
 
-void bitmap_set(bitmap_t *bitmap, int bit)
+__attribute__((always_inline))
+inline void bitmap_set(bitmap_t *bitmap, int bit)
 {
     bitmap->map[bit / 32] |= (1 << (bit % 32));
 }
 
-void bitmap_clear(bitmap_t *bitmap, int bit)
+__attribute__((always_inline))
+inline void bitmap_clear(bitmap_t *bitmap, int bit)
 {
     bitmap->map[bit / 32] &= ~(1 << (bit % 32));
 }
 
-uint32_t bitmap_get(bitmap_t *bitmap, int bit)
+__attribute__((always_inline))
+inline uint32_t bitmap_get(bitmap_t *bitmap, int bit)
 {
     return bitmap->map[bit / 32] & (1 << (bit % 32));
 }
