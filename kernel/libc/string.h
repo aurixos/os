871d3dd03c3c9f834e2f073469e5c9d2a201c588
Jozef Nagy <schkwve@gmail.com>
2024-04-24 20:32:20 +0200

    Implemented strcmp and strncmp in kernel libc

--- kernel/libc/string.h
+++ kernel/libc/string.h
@@ -1,9 +1,14 @@
 #ifndef _STRING_H
 #define _STRING_H
 
+// @todo: get rid of this
+#include <stdint.h>
+
 int memcmp(const void *, const void *, size_t);
 void *memcpy(void *, const void *, size_t);
 void *memmove(void *, const void *, size_t);
 void *memset(void *, int, size_t);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
 
 #endif /* _STRING_H */
