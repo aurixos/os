96f8d0803ea44196634aa4ba3a528601e84bd21c
Jozef Nagy <schkwve@gmail.com>
2024-04-26 15:28:14 +0200

    feat: Make backtrace code better understandable

--- kernel/arch/x86_64/cpu/backtrace.c
+++ kernel/arch/x86_64/cpu/backtrace.c
@@ -9,29 +9,30 @@ void print_backtrace(void)
 {
 	uint64_t *base;
 	__asm__ volatile("mov %%rbp, %0" : "=g"(base) :: "memory");
+	struct stackframe *frame = (struct stackframe *)*base;
 
 	while (1) {
-		base = (uint64_t *)*base;
-		uint64_t return_addr = *(base + 1);
-		
 		// 0 is pushed to the stack before calling kernel entry point
-		if (return_addr == (uint64_t)NULL || base == NULL || return_addr < (uint64_t)0xffffffff80000000) {
+		if (frame->rip == (uint64_t)NULL || base == NULL || frame->rip < (uint64_t)0xffffffff80000000) {
 			break;
 		}
 
 		// see if the return address is in our symbol table
 		int idx = -1;
 		for (int i = 0; _symtab[i].addr < UINTPTR_MAX; i++) {
-			if (_symtab[i].addr < return_addr && _symtab[i+1].addr >= return_addr) {
+			if (_symtab[i].addr < frame->rip && _symtab[i+1].addr >= frame->rip) {
 				idx = i;
 			}
 		}
 	
 		if (idx < 0) {
-			_klog("    [%.16lx]  <\?\?\?>\n", return_addr);
+			_klog("    [%.16lx]  <\?\?\?>\n", frame->rip);
 		} else {
-			_klog("    [%.16lx]  <%s+0x%04x>\n", return_addr, _symtab[idx].name, return_addr - _symtab[idx].addr);
+			_klog("    [%.16lx]  <%s+0x%04x>\n", frame->rip, _symtab[idx].name, frame->rip - _symtab[idx].addr);
 		}
+
+		// proceed to the next trace
+		frame = frame->rbp;
 	}
 
 	_klog("End of trace.\n");
--- kernel/include/arch/x86_64/cpu/cpu.h
+++ kernel/include/arch/x86_64/cpu/cpu.h
@@ -3,4 +3,9 @@
 
 #include "cpu_utils.h"
 
+struct stackframe {
+	struct stackframe *rbp;
+	uint64_t rip;
+} __attribute__((packed));
+
 #endif /* _CPU_CPU_H */
