f61a398950c36ba8abed44d90d280ec679a919c4
Jozef Nagy <schkwve@gmail.com>
2024-05-09 16:09:57 +0200

    fix: Update OVMF image and x86_64 QEMU flags

--- arch/x86_64/config.mk
+++ arch/x86_64/config.mk
@@ -1,5 +1,5 @@
 QEMU := qemu-system-x86_64
-QEMU_ARCH_FLAGS := -bios arch/$(ARCH)/ovmf_x64.fd
+QEMU_ARCH_FLAGS := -M q35,smm=off -bios arch/$(ARCH)/ovmf_x64.fd
 
 ARCH_RELEASE_HDD_RECOMMENDED_SIZE := 256
 ARCH_RELEASE_SDCARD_RECOMMENDED_SIZE := 256
