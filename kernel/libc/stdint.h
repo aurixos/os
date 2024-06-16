664be06d87124f89ed011060800a03e368c79d19
Jozef Nagy <schkwve@gmail.com>
2024-06-16 19:32:49 +0200

    feat: Introduced new standard types in stdint.h

--- kernel/libc/stdint.h
+++ kernel/libc/stdint.h
@@ -3,4 +3,16 @@
 
 #include <sys/_types.h>
 
+typedef int8_t i8;
+typedef uint8_t u8;
+typedef int16_t i16;
+typedef uint16_t u16;
+typedef int32_t i32;
+typedef uint32_t u32;
+typedef int64_t i64;
+typedef uint64_t u64;
+
+typedef size_t usize;
+typedef ssize_t isize;
+
 #endif /* _STDINT_H */
